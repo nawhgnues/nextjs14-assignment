00bf23fc3988a0ad8d03e9d36bf1c185205f7fcc
SEUNGHWAN LEE <123640210+nawhgnues@users.noreply.github.com>
2024-07-06 01:48:40 +0900

    Install the zod

--- src/app/login/action.ts
+++ src/app/login/action.ts
@@ -1,7 +1,6 @@
 "use server";
 
 export const handleForm = async (previousState: any, formData: FormData) => {
-  "use server";
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
   if (formData.get("password") === "12345") {
